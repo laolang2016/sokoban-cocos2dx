0dcfb9f721e686b0f3f79b53def4a4ea27cbad67
laolang2016 <xiaodaima2016@163.com>
2023-06-09 11:25:56 +0800

    ✨ feat: 添加素材并测试

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -26,11 +26,38 @@ bool HelloWorld::init()
 	const Vec2  origin = Director::getInstance()->getVisibleOrigin();
 	log("x:%f , y:%f", origin.x, origin.y);
 
-	auto titleLabel = Label::createWithTTF(CommonUtil::GBKToUTF8("推箱子"), CommonUtil::GetDefaultFontPath(), 24);
-	const auto x = visibleSize.width / 2;
-	const auto y = origin.y + visibleSize.height - titleLabel->getContentSize().height;
-	titleLabel->setPosition(Vec2(x, y));
-	this->addChild(titleLabel, 1);
+	auto spritecache = SpriteFrameCache::getInstance();
+	spritecache->addSpriteFramesWithFile("texture_packer/sprite.plist", "texture_packer/sprite.png");
+
+	auto wall = Sprite::createWithSpriteFrameName("wall.png");
+	wall->setContentSize(Size(48, 48));
+	wall->setPosition(Vec2(200 + 48 * 0, 200));
+	this->addChild(wall, 1);
+
+	auto box_blue = Sprite::createWithSpriteFrameName("box_blue.png");
+	box_blue->setContentSize(Size(48, 48));
+	box_blue->setPosition(Vec2(200 + 48 * 1 + 1, 200));
+	this->addChild(box_blue, 1);
+
+	auto box_red = Sprite::createWithSpriteFrameName("box_red.png");
+	box_red->setContentSize(Size(48, 48));
+	box_red->setPosition(Vec2(200 + 48 * 2 + 2, 200));
+	this->addChild(box_red, 1);
+
+	auto ball_blue = Sprite::createWithSpriteFrameName("ball_blue.png");
+	ball_blue->setContentSize(Size(48, 48));
+	ball_blue->setPosition(Vec2(200 + 48 * 3 + 1, 200));
+	this->addChild(ball_blue, 1);
+
+	auto ball_red = Sprite::createWithSpriteFrameName("ball_red.png");
+	ball_red->setContentSize(Size(48, 48));
+	ball_red->setPosition(Vec2(200 + 48 * 3 + 1, 200));
+	this->addChild(ball_red, 1);
+
+	auto charater = Sprite::createWithSpriteFrameName("charater.png");
+	charater->setContentSize(Size(48, 48));
+	charater->setPosition(Vec2(200 + 48 * 4 + 1, 200));
+	this->addChild(charater, 1);
 
 	return true;
 }
